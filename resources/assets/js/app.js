85cb08aaf2177d3726b37f170cac769875d4d4e3
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2018-01-28 10:40:47 +0100

    Connecting the user to the API and retrieving its information

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,5 +1,7 @@
 fetch('/api/user', {
+    credentials: 'same-origin',
     headers: {
-        'X-Requested-With': 'XMLHttpRequest'
+        'X-Requested-With': 'XMLHttpRequest',
+        'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
     }
 })
